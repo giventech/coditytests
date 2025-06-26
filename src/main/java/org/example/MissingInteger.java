066ffc3f598c80661a4ff8fa87a939e762b41fca
fabrice <giventech@gmail.com>
2025-06-26 17:57:11 +1000

    Both Solutions

--- src/main/java/org/example/MissingInteger.java
+++ src/main/java/org/example/MissingInteger.java
@@ -1,16 +1,19 @@
 package org.example;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class MissingInteger {
     public static void main(String[] args) {
-        //Write an initial value for and array and run solution method, make sure that you can print the result
-        int[] A = {-1, -3, 4, 0, 1, 2};
-        // int[] A = {1, 2,3, 5};
+     //Write an initial value for and array and run solution method, make sure that you can print the result
+        int[] A = {1, 3, 6, 4, 1, 2};
         //Print the result of the solution method
         System.out.println("The missing integer is: " + solution(A));
+        System.out.println("The missing integer is: " + solutionWithHashSet(A));
+
 
 
 
@@ -35,5 +38,20 @@ public class MissingInteger {
             }
         }
         return missingInteger;
+    }
+
+    public static int solutionWithHashSet(int[] A) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : A) {
+            if (num > 0) {
+                set.add(num);
+            }
+        }
+        for (int i = 1; i <= set.size() + 1; i++) {
+            if (!set.contains(i)) {
+                return i;
+            }
+        }
+        return 1; // This line is a fallback, though logically it might not be needed.
     }
 }
